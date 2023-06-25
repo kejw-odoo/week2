bce1de498770aec27c6d5bb10695b28506bf2230
kejw-odoo <kejw@odoo.com>
2023-06-25 23:32:39 +0000

    add new module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': """
+                Motorcycle Registry
+                ====================
+                This Module is used to keep track of the 
+                Motorcycle Registration and Ownership of each 
+                motorcycled of the brand.
+            """,
+    'author': 'kejw-odoo',
+    'website': 'https://github.com/kejw-odoo',
+    'category': 'Kawiil',
+    'application': True
+}
