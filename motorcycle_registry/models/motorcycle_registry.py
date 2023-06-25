511c194e20ac4c30fecfb632852d0935b0a26c48
kejw-odoo <odoo@ampd02.odoo.com>
2023-06-25 23:58:37 +0000

    added models

---
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+class MotorcycleRegistry(models.Model):
+    _name = 'motorcycle.registry'
+    _description = 'Motorcycle Registry'
+    
+    registry_number = fields.Char('Registry Number', required=True)
+    vin = fields.Char(string='VIN', required=True)
+    first_name = fields.Char(string='First Name', required=True)
+    last_name = fields.Char(string='Last Name', required=True)
+    picture = fields.Image(string='Photograph')
+    current_mileage = fields.Float(string='Current Mileage')
+    license_plate = fields.Char(string='License Plate Number')
+    certificate_title = fields.Binary(string='Certificate of Title')
+    registry_date = fields.Date(string='Registration Date')
