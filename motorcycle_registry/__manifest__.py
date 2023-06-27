0cd94ebc1549b917bfa27130e67914cebdf7779c
kejw-odoo <odoo@ampd02.odoo.com>
2023-06-27 18:37:32 +0000

    actions

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -14,6 +14,7 @@
     "data": [
         "security/motorcycle_registry_groups.xml",
         "security/ir.model.access.csv",
+        "views/motorcycle_registry_menuitems.xml"
     ],
     "demo": [
         "demo/motorcycle_registry_demo.xml",
