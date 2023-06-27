e4790643a44276498776f76d9353abab70990ba0
kejw-odoo <odoo@ampd02.odoo.com>
2023-06-27 19:11:56 +0000

    new_views

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -14,7 +14,8 @@
     "data": [
         "security/motorcycle_registry_groups.xml",
         "security/ir.model.access.csv",
-        "views/motorcycle_registry_menuitems.xml"
+        "views/motorcycle_registry_menuitems.xml",
+        "views/motorcycle_registry_views.xml",
     ],
     "demo": [
         "demo/motorcycle_registry_demo.xml",
